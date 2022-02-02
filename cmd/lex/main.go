ea141d446c9cfab807682839640fc833758a3a07
Minizbot2012 <Mini@zbots.org>
2022-02-02 07:11:19 -0500

    ALL: Replacer Functions

--- cmd/lex/main.go
+++ cmd/lex/main.go
@@ -5,28 +5,94 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"path/filepath"
+
+	devenv "github.com/Minizbot2012/DevEnv"
 )
 
-var exetab map[string]string
+type vars struct {
+	Name  string
+	Value string
+}
+
+type Env struct {
+	//add to environment
+	Environ []vars `json:"vars"`
+	//append to PATH
+	PathAppend []string `json:"path"`
+	//Vars to keep and pass
+	Passthrough []string `json:"keep"`
+	//Terminal to use
+	Terminal string
+}
 
 func main() {
-	ex, _ := os.Executable()
-	exPath := filepath.Dir(ex)
-	jsf, err := os.Open(path.Join(exPath, "lex.json"))
+	//Init config file
+	jsf, err := os.Open(path.Join(devenv.CWD, "config_"+devenv.OS+".json"))
+	if err != nil {
+		jsf, err = os.Open(path.Join(devenv.CWD, "config.json"))
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer jsf.Close()
+	var environ Env
+	//Decode config file
+	err = json.NewDecoder(jsf).Decode(&environ)
 	if err != nil {
 		panic(err)
 	}
-	var cmd *exec.Cmd
-	json.NewDecoder(jsf).Decode(&exetab)
-	if exe, ok := exetab[os.Args[0]]; ok {
-		cmd = exec.Command(path.Join(exPath, exe), os.Args[1:]...)
-	} else if exe, ok := exetab[os.Args[1]]; ok {
-		cmd = exec.Command(path.Join(exPath, exe), os.Args[2:]...)
+	// Default passthrough
+	environ.Passthrough = append(environ.Passthrough, "SSH_AUTH_SOCK")
+	if devenv.OS == "windows" {
+		environ.Passthrough = append(environ.Passthrough,
+			"PROCESSOR_ARCHITECTURE", "PROCESSOR_IDENTIFIER",
+			"PSModulePath", "PATHEXT", "OS", "DriverData", "NUMBER_OF_PROCESSORS", "USERNAME",
+			"APPDATA", "PROGRAMFILES", "ProgramFiles(x86)", "ProgramData")
+	} else if devenv.OS == "linux" {
+		environ.Passthrough = append(environ.Passthrough,
+			"DISPLAY", "WAYLAND_DISPLAY", "LANG", "HOME", "XDG_RUNTIME_DIR",
+			"XDG_SESSION_TYPE", "XDG_SESSION_DESKTOP", "USER", "SHELL", "SESSION_MANAGER",
+			"TERM", "XAUTHORITY", "DESKTOP_SESSION", "HISTCONTROL", "COLORTERM",
+			"DBUS_SESSION_BUS_ADDRESS")
 	}
-	if cmd != nil {
-		cmd.Stdout, _ = os.OpenFile(os.DevNull, os.O_WRONLY, 0755)
-		cmd.Stderr, _ = os.OpenFile(os.DevNull, os.O_WRONLY, 0755)
-		cmd.Start()
+	//do our replacements
+	//$PWD
+
+	for i, v := range environ.Environ {
+		environ.Environ[i].Value = devenv.Replace(v.Value)
 	}
+	for i, v := range environ.PathAppend {
+		environ.PathAppend[i] = devenv.Replace(v)
+	}
+
+	//Initiate Terminal
+	term, err := exec.LookPath(environ.Terminal)
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(term)
+	cmd.Env = []string{}
+	path := os.Getenv("PATH")
+
+	//Path Appends
+	for _, v := range environ.PathAppend {
+		path = path + v + string(os.PathListSeparator)
+	}
+	cmd.Env = append(cmd.Env, "PATH="+path)
+
+	//Environment Variables
+	for _, v := range environ.Environ {
+		cmd.Env = append(cmd.Env, v.Name+"="+v.Value)
+	}
+
+	//Passthrough Variables
+	for _, v := range environ.Passthrough {
+		cmd.Env = append(cmd.Env, v+"="+os.Getenv(v))
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
---
+++ replacer.go
@@ -0,0 +1,43 @@
+package devenv
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var CWD string
+var Username string
+var Home string
+var OS string
+var Arch string
+var ExeDir string
+
+func init() {
+	CWD, _ = os.Getwd()
+	user, _ := user.Current()
+	ex, _ := os.Executable()
+	ExeDir = filepath.Dir(ex)
+	Username = user.Username
+	Home = user.HomeDir
+	Arch = runtime.GOARCH
+	OS = runtime.GOOS
+}
+
+//$(PWD) -> getwd
+//$(OS) -> runtime.GOOS
+//$(ARCH) -> runtime.GOARCH
+//$(USER) -> user.Current().Username
+//$(HOME) -> user.Current().HomeDir
+//$(EDIR) -> Exe's dir
+func Replace(str string) (ret string) {
+	ret = str
+	ret = strings.ReplaceAll(ret, `$(PWD)`, CWD)
+	ret = strings.ReplaceAll(ret, `$(OS)`, OS)
+	ret = strings.ReplaceAll(ret, `$(ARCH)`, Arch)
+	ret = strings.ReplaceAll(ret, `$(USER)`, Username)
+	ret = strings.ReplaceAll(ret, `$(EDIR)`, ExeDir)
+	return ret
+}
