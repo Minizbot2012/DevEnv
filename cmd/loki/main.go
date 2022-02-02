31603bf6c6a46fa0fab53c3dd6dbe68e3c6c189d
Minizbot2012 <bradleyfwin@gmail.com>
2022-02-02 07:11:19 -0500

    ALL: Replacer Functions

---
+++ cmd/loki/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"os/exec"
+	"path"
+
+	devenv "github.com/Minizbot2012/DevEnv"
+)
+
+var exetab map[string]string
+
+func main() {
+	jsf, err := os.Open(path.Join(devenv.ExeDir, "loki.json"))
+	if err != nil {
+		panic(err)
+	}
+	var cmd *exec.Cmd
+	json.NewDecoder(jsf).Decode(&exetab)
+	if exe, ok := exetab[os.Args[0]]; ok {
+		exe = devenv.Replace(exe)
+		if path.IsAbs(exe) {
+			cmd = exec.Command(exe, os.Args[1:]...)
+		} else {
+			cmd = exec.Command(path.Join(devenv.ExeDir, exe), os.Args[1:]...)
+		}
+	} else if exe, ok := exetab[os.Args[1]]; ok {
+		exe = devenv.Replace(exe)
+		if path.IsAbs(exe) {
+			cmd = exec.Command(exe, os.Args[2:]...)
+		} else {
+			cmd = exec.Command(path.Join(devenv.ExeDir, exe), os.Args[2:]...)
+		}
+	}
+	if cmd != nil {
+		cmd.Stdout, _ = os.OpenFile(os.DevNull, os.O_WRONLY, 0755)
+		cmd.Stderr, _ = os.OpenFile(os.DevNull, os.O_WRONLY, 0755)
+		cmd.Start()
+	}
+}
